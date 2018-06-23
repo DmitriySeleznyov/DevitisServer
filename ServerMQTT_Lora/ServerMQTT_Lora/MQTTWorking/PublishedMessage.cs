e7c0e2a7e546039f6064f2a05624fd36c1c14f73
Dmitriy Seleznyov <7elezniovdima@gmail.com>
2018-06-23 12:50:51 +0300

    Fixed small bags.

--- ServerMQTT_Lora/ServerMQTT_Lora/MQTTWorking/PublishedMessage.cs
+++ ServerMQTT_Lora/ServerMQTT_Lora/MQTTWorking/PublishedMessage.cs
@@ -14,31 +14,6 @@ namespace ServerMQTT_Lora
     public class PublishedMessage
     {
         MqttClient client;
-        delegate void SetTextCallback(string text);
-
-
-        public void PublishMess()
-        {
-                client = new MqttClient("broker.hivemq.com");
-                byte code = client.Connect(Guid.NewGuid().ToString());
-                client.ProtocolVersion = MqttProtocolVersion.Version_3_1;
-
-            if (client.IsConnected)
-            {   
-                Console.WriteLine("Connected to Mqtt: ");
-                ushort msgId = client.Subscribe(new string[] { "exz/lora" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
-                client.MqttMsgPublishReceived += new MqttClient.MqttMsgPublishEventHandler(EventPublished);
-            }
-            else
-            {
-                Console.WriteLine("Cannot connect to Mqtt. Reload.");
-            }
-        }
-        
-        private void EventPublished(Object sender, MqttMsgPublishEventArgs e)
-        {
-        
-        delegate void SetTextCallback(string text);
         /// <summary>
         /// Method getting message from Mqtt broker.
         /// </summary>
